392066fbb314c1a754e09c19ee4846a5bf6ae029
Binaya Baral <binaya.baral5@gmail.com>
2020-06-26 09:08:07 +0545

    production

---
+++ build/precache-manifest.c15adfd177844ce87707f98a0088400a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4db0c8b63b13cb61e95af0c4df1a8c8f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bbb15cd317bf4e58efd0",
+    "url": "/static/css/main.122ec586.chunk.css"
+  },
+  {
+    "revision": "957fa993ea0058eabfab",
+    "url": "/static/js/2.9ac065ae.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/static/js/2.9ac065ae.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bbb15cd317bf4e58efd0",
+    "url": "/static/js/main.66794252.chunk.js"
+  },
+  {
+    "revision": "2e075117383e7b156060",
+    "url": "/static/js/runtime-main.2f0bb968.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/static/media/img01.d7265326.png"
+  }
+]);
